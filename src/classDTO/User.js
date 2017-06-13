4ffe76e0bfe2e8f49c805f91c32a89fd1f2d0a49
hduss <delmas.theo@laposte.net>
2017-06-13 16:57:59 +0200

    addclass User (DTO)

---
+++ src/classDTO/User.js
@@ -0,0 +1,26 @@
+class User{
+
+	constructor(firstName, lastName, mail, pseudo, password, birthday, informations, picture, isModerator, isAdmin, sexe){
+
+		this.firstName = firstname;
+		this.lastName = lastName;
+		this.mail = mail;
+		this.pseudo = pseudo;
+		this.password = password;
+		this.birthday = birthday;
+		this.informations = informations;
+		this.picture = picture;
+		this.isModerator = isModerator;
+		this.isAdmin = isAdmin;
+		this.sexe = sexe;
+	}
+
+	getPassword() {
+		return this.password;
+	}
+
+
+
+}
+
+module.exports = User;
